1b6348e5068bbc73a231dbf91190cbc4e1f83839
Kokulapalan Wimalanathan <kokul@iastate.edu>
2019-05-24 20:47:57 -0500

    working scripts to build containers

--- build-GOMAP.sh
+++ build-GOMAP.sh
@@ -5,13 +5,13 @@ export SINGULARITY_TMPDIR="/home/gokul/lab_data/dill-picl/GOMAP/GOMAP-singularit
 instance_name="GOMAP"
 if [ -f "$instance_name.simg" ]
 then
-    sudo rm -r "$instance_name.simg"
+    rm -rf "$instance_name"
+    rm -r "$instance_name.simg"
 fi
 
 if [ ! -d $instance_name ]
 then
-time singularity -v build -s $instance_name singularity/Singularity.v1.2.mpich-3.2.1
+    time singularity -v build -s $instance_name singularity/Singularity.v1.2.mpich-3.2 && \
+    time singularity -v build $instance_name.simg $instance_name && \
+    chown gokul:gokul $instance_name.simg
 fi
-
-time singularity -v build $instance_name.simg $instance_name && \
-chown gokul:gokul $instance_name.simg
