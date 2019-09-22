423a6483c512a9c79ff75acf7cc0377151695ed9
Kokulapalan Wimalanathan <kokul@iastate.edu>
2019-09-22 08:04:04 -0500

    updated container to 1.3 version

--- build-GOMAP.sh
+++ build-GOMAP.sh
@@ -1,15 +1,13 @@
 #export SINGULARITY_CACHEDIR="/home/gokul/lab_data/dill-picl/GOMAP/GOMAP-singularity"
-export SINGULARITY_TMPDIR="/home/gokul/lab_data/dill-picl/GOMAP/GOMAP-singularity"
+#export SINGULARITY_TMPDIR="/home/gokul/lab_data/dill-picl/GOMAP/GOMAP-singularity"
 #export SINGULARITY_LOCALCACHEDIR="/home/gokul/lab_data/dill-picl/GOMAP/GOMAP-singularity"
 
 instance_name="GOMAP"
 
-rm -rf "$instance_name"
 rm -r "$instance_name.simg"
 
 if [ ! -d $instance_name ]
 then
-    time singularity -v build -s $instance_name singularity/Singularity.v1.2.mpich-3.2.1 && \
-    time singularity -v build $instance_name.simg $instance_name && \
+    time singularity -v build --tmpdir $PWD/tmp $instance_name.simg singularity/Singularity.v1.3.mpich-3.2.1 && \
     chown gokul:gokul $instance_name.simg
 fi
