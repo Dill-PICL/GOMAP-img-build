d7bca310bb98c452aa04fc1dd58cf4659a4af58c
Kokulapalan Wimalanathan <kokulapalan@gmail.com>
2019-10-13 21:12:46 -0400

    changing the script to bash

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 instance_name="GOMAP"
-if [ -f "$instance_name.sif" ]
-then
-    rm -r "$instance_name.sif"
-fi
-
 azcopy cp https://gomap.blob.core.windows.net/gomap/gomap/GOMAP-base/1.3.1/GOMAP-base.sif > GOMAP-base.sif
-
-if [ ! -d $instance_name ]
-then
-    sudo mkdir tmp
-    sudo singularity build --tmpdir tmp $instance_name.sif singularity/Singularity.v1.3.1.mpich-3.2.1
-fi
+mkdir tmp
+sudo singularity build --tmpdir tmp $instance_name.sif singularity/Singularity.v1.3.1.mpich-3.2.1
