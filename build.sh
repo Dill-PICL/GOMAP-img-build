458ec5708cb9cdcaeef5cb6bef9150be584485e9
Wimalanathan Kokulapalan <wkpalan@users.noreply.github.com>
2020-10-31 09:07:01 -0400

    adding the correct copying instructions

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ instance_name="GOMAP"
 azcopy cp https://gomap.blob.core.windows.net/gomap/gomap/GOMAP-base/1.3.1/GOMAP-base.sif > GOMAP-base.sif
 ls -lh
 sudo mkdir tmp
-sudo singularity build --tmpdir tmp $instance_name.sif singularity/Singularity.v1.3.1.mpich-3.2.1
+sudo singularity build --tmpdir tmp ${IMAGE}.sif singularity/Singularity.mpich-3.2.1
