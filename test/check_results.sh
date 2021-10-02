70f1916993195156abe0f3345bfd77ba9123ea12
Ubuntu <azureuser@kw-research.svv3lf4tqdjujbwoplciulkjwa.bx.internal.cloudapp.net>
2021-10-02 20:51:13 +0000

    adding the initial files from v1.3.7

--- test/check_results.sh
+++ test/check_results.sh
@@ -1,3 +1,9 @@
-cut -f 3,5 GOMAP-0.3_GOMAP-input/gaf/e.agg_data/0.3_GOMAP-input.aggregate.gaf | sort > results/new.txt
-cut -f 3,5 0.3_GOMAP-input.aggregate.gaf  | sort > results/old.txt
-diff results/new.txt results/old.txt
+cut -f 3,5 test/GOMAP-0.3_GOMAP-input/gaf/e.agg_data/0.3_GOMAP-input.aggregate.gaf | sort > test/results/new.txt
+cut -f 3,5 test/results/0.3_GOMAP-input.aggregate.gaf  | sort > test/results/old.txt
+num_lines=`diff test/results/new.txt test/results/old.txt | wc -l`
+
+if [ $num_lines -gt 0 ]
+then
+	echo "test failed"
+	exit 1
+fi
